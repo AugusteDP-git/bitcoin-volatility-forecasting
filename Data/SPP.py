392350b5296b58bade189d3ed5d3138f811fa347
AugusteDP-git <auguste.darracq@gmail.com>
2025-06-06 13:23:50 +0200

    Update

--- Data/SPP.py
+++ Data/SPP.py
@@ -3,12 +3,10 @@ import statsmodels.api as sm
 
 PATH = "Enter your path here"
 
-# Load the data from Huggingface file
 url = "https://huggingface.co/datasets/danilocorsi/LLMs-Sentiment-Augmented-Bitcoin-Dataset/resolve/main/merged/merged_daily.csv"
 df = pd.read_csv(url)
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-# Filter between June 4, 2018 and September 30, 2018
 filtered_df = df[(df['timestamp'] >= "2018-06-04") & (df['timestamp'] <= "2018-09-30")]
 filtered_df= filtered_df[['timestamp', 'fng_value', 'cbbi_value']]
 filtered_df['Aggregate sentiment'] = (filtered_df['fng_value'] + filtered_df['cbbi_value']) / 2
